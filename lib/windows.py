11fa0b364703782316cbcc68203f5baddc4433f5
LlGHTNlNG <leninzz020@gmail.com>
2024-10-17 14:35:12 -0400

    [fix] - allow edit the papersize
    allows you to edit the printer paper type automatically to change the printer when printing.

--- lib/windows.py
+++ lib/windows.py
@@ -9,8 +9,25 @@ class Printing(object):
         pdf_file = pdf.generate(printer_data, orientation)
         print(printer)
         win32print.SetDefaultPrinter(printer)
+        
+        printer_handle = win32print.OpenPrinter(printer)
+        printer_info = win32print.GetPrinter(printer_handle, 2)
+        devmode = printer_info['pDevMode']
+        
+        # rundll32 printui.dll,PrintUIEntry /e /n "EPSON LX-350 ESC/P" # Usar en caso de emergecia
+        if orientation == 'landscape':
+            devmode.PaperSize = 75  # "Fanfold 11 x 8 1/2 in"
+        elif orientation == 'portrait':
+            devmode.PaperSize = 1 # "Letter"
+            
+        try:
+            win32print.SetPrinter(printer_handle, 2, printer_info, 0)
+        except:
+            pass
+        
+        win32print.ClosePrinter(printer_handle)
         win32api.ShellExecute(0, "print", pdf_file, None, ".", 0)
-
+        
         time.sleep(8)
         
         self.close_adobe_reader()
