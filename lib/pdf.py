bb9e6e3ac9939c7a6910a33274699a9f5ccbdb4d
LlGHTNlNG <leninzz020@gmail.com>
2024-10-17 10:31:09 -0400

    [add] - new features for multi printer

--- lib/pdf.py
+++ lib/pdf.py
@@ -7,14 +7,18 @@ def generate(printer_data, orientation):
     current_working_directory = os.getcwd()
     pdf_dir = os.path.join(current_working_directory, "pdf")
     
-    # Create directory if it does not exist
     os.makedirs(pdf_dir, exist_ok=True)
     
     filename = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + str(uuid.uuid4()) + '.pdf'
     pdf_file = os.path.join(pdf_dir, filename)
     
-    # Define the HTML content
-    html_content = f"""
+    
+    # Configuration for wkhtmltopdf
+    path_wkhtmltopdf = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'  # Update this path as needed
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    
+    if orientation == 'landscape':
+        html_content = f"""
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -29,13 +33,7 @@ def generate(printer_data, orientation):
     </body>
     </html>
     """
-    print(printer_data)
     
-    # Configuration for wkhtmltopdf
-    path_wkhtmltopdf = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'  # Update this path as needed
-    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-    
-    if orientation == 'landscape':
         options = {
             'orientation': f'{orientation}',
             'margin-top': '0in',
@@ -44,6 +42,22 @@ def generate(printer_data, orientation):
             'margin-left': '0.4in',
         }
     elif orientation == 'portrait':
+        html_content = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {{ font-size: 17px; font-family: 'Calibri', sans-serif;}}
+        </style>
+    </head>
+    <body>
+        {printer_data}
+    </body>
+    </html>
+    """
+    
         options = {
             'orientation': f'{orientation}',
             'margin-top': '0in',
@@ -51,6 +65,8 @@ def generate(printer_data, orientation):
             'margin-bottom': '0in',
             'margin-left': '0.3in',
         }
+        
+    print(printer_data)
     
     # Convert HTML to PDF
     pdfkit.from_string(html_content, pdf_file, configuration=config, options=options)
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_cors import CORS, cross_origin
+import win32print
+import subprocess
 
 if platform == 'darwin':
     from lib.macos import Printing
@@ -16,24 +18,22 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/dotmatrix/print', methods=['POST'])
 def index():
     
-    config = dotenv_values(".env")
-    if request.form['orientation'] in ['landscape']:
-        printer = config['PRINTER_NAME_LANDSCAPE']
-    elif request.form['orientation'] in ['portrait']:
-        printer = config['PRINTER_NAME_PORTRAIT']
-    
     PrintingQueu = Printing()
-    PrintingQueu.print(request.form['printer_data'], request.form['orientation'], printer)
+    PrintingQueu.print(request.form['printer_data'], request.form['orientation'], request.form['printer'])
     out = {'status': 'OK'}
     return jsonify(out)
 
-@app.route('/', methods=['GET'])
-def home():
-    config = dotenv_values(".env")
-    out = {'status': 'OK',
-           'printer': config['PRINTER_NAME'],
-           'os': platform,}
-    return jsonify(out)
+@app.route('/printers', methods=['GET'])
+def get_printers():
+    printers = []
+    if platform == 'Darwin':
+        result = subprocess.run(['lpstat', '-p'], stdout=subprocess.PIPE)
+        printers = [line.split(' ')[1] for line in result.stdout.decode().split('\n') if line] 
+    else:
+        printers = [printer[2] for printer in win32print.EnumPrinters(2)]
+        print(printers)
+
+    return jsonify({'printers': printers})
 
 if __name__ == '__main__':
     app.run(port=8000)
