ecf7f858607c9c92c81a36f970d5ec2189570486
LlGHTNlNG <leninzz020@gmail.com>
2024-10-17 10:31:09 -0400

    [add] - new features for multi printer

--- lib/windows.py
+++ lib/windows.py
@@ -1,15 +1,25 @@
 import win32api
 import win32print
+import psutil
+import time
 from lib import pdf
 
 class Printing(object):
     def print(self,printer_data, orientation, printer):
         pdf_file = pdf.generate(printer_data, orientation)
-        #rotated_pdf_file = rotatepdf.rotate_pdf(pdf_file)
+        print(printer)
         win32print.SetDefaultPrinter(printer)
         win32api.ShellExecute(0, "print", pdf_file, None, ".", 0)
 
-
+        time.sleep(8)
+        
+        self.close_adobe_reader()
 
     def get_printers(self):
         return win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL, None, 1)
+    
+    def close_adobe_reader(self):
+        for proc in psutil.process_iter(['pid', 'name']):
+            if proc.info['name'] and 'Acrobat.exe' in proc.info['name']:
+                proc.terminate()
+                proc.wait(timeout=8) 
