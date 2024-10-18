7b80ec3d111aadd30fd886e534fa899e50f8670a
LlGHTNlNG <leninzz020@gmail.com>
2024-10-18 10:41:41 -0400

    [fix] - refactor code

--- lib/pdf.py
+++ lib/pdf.py
@@ -12,44 +12,34 @@ def generate(printer_data, orientation):
     filename = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + str(uuid.uuid4()) + '.pdf'
     pdf_file = os.path.join(pdf_dir, filename)
     
-    
     # Configuration for wkhtmltopdf
     path_wkhtmltopdf = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'  # Update this path as needed
     config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
     
     if orientation == 'landscape':
-        html_content = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body {{ font-size: 13px; font-family: 'Courier New', Courier, monospace;}}
-        </style>
-    </head>
-    <body>
-        {printer_data}
-    </body>
-    </html>
-    """
-    
-        options = {
-            'orientation': f'{orientation}',
-            'margin-top': '0in',
-            'margin-right': '0.78in',
-            'margin-bottom': '0in',
-            'margin-left': '0.4in',
-        }
+        margin_top = '0in',
+        margin_right = '0.78in',
+        margin_bottom = '0in',
+        margin_left = '0.4in',
+        font_size = '13px'
+        font_family = "'Courier New', Courier, monospace"
+        
     elif orientation == 'portrait':
-        html_content = f"""
+        margin_top = '0in',
+        margin_right = '0.3in',
+        margin_bottom = '0in',
+        margin_left = '0.3in',
+        font_size = '17px'
+        font_family = "'Calibri', sans-serif"
+       
+    html_content = f"""
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-            body {{ font-size: 17px; font-family: 'Calibri', sans-serif;}}
+            body {{ font-size: {font_size}; font-family: {font_family};}}
         </style>
     </head>
     <body>
@@ -58,13 +48,14 @@ def generate(printer_data, orientation):
     </html>
     """
     
-        options = {
-            'orientation': f'{orientation}',
-            'margin-top': '0in',
-            'margin-right': '0.3in',
-            'margin-bottom': '0in',
-            'margin-left': '0.3in',
-        }
+    options = {
+        'orientation': f'{orientation}',
+        'margin-top': margin_top,
+        'margin-right': margin_right,
+        'margin-bottom': margin_bottom,
+        'margin-left': margin_left,
+    }   
+        
         
     print(printer_data)
     
