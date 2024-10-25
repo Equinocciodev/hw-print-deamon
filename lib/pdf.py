73505c7e0a46c075677304d63d4912afb964d063
grupoclip <124650489+grupoclip@users.noreply.github.com>
2024-10-25 15:11:18 -0400

    ✨ feat(`lib/macos.py`): Add `do_print_old` method to handle legacy print requests. ✨ feat(`main.py`): Update printer configuration retrieval for compatibility with new form structure.

--- lib/pdf.py
+++ lib/pdf.py
@@ -28,6 +28,7 @@ def generate(form):
     font_size = '17px'
     font_family = "'Calibri', sans-serif"
     orientation = 'portrait'
+    letter_spacing = 'auto'
     if str(form.get('orientation')).lower() == 'landscape':
         orientation = 'landscape'
         margin_top = '0in'
@@ -36,6 +37,7 @@ def generate(form):
         margin_left = '0.4in'
         font_size = '13px'
         font_family = "'Courier New', Courier, monospace"
+        letter_spacing = 'auto'
 
     margin_top = margin_top if not form.get('margin_top') else form.get('margin_top') + 'mm'
     margin_right = margin_right if not form.get('margin_right') else form.get('margin_right') + 'mm'
@@ -43,6 +45,7 @@ def generate(form):
     margin_bottom = margin_bottom if not form.get('margin_bottom') else form.get('margin_bottom') + 'mm'
     font_family = font_family if not form.get('font_family') else form.get('font_family')
     font_size = font_size if not form.get('font_size') else form.get('font_size') + 'px'
+    letter_spacing = letter_spacing if not form.get('letter_spacing') else form.get('letter_spacing') + 'px'
 
     html_content = f"""
     <!DOCTYPE html>
@@ -51,7 +54,7 @@ def generate(form):
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-            body {{ font-size: {font_size}; font-family: {font_family};}}
+            body {{ font-size: {font_size}; font-family: {font_family};letter-spacing: {letter_spacing};}}
         </style>
     </head>
     <body>
